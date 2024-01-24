4840f57e51cc1786b8249e8a6110f60e5d6de1cf
Luis Vazquez Franco <luis.vazquez.franco.al@iespoligonosur.org>
2024-01-24 14:44:36 +0100

    revisar busqueda simple

--- Huerto/api_urls.py
+++ Huerto/api_urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .api_views import *
 urlpatterns = [
     path('huertos',huerto_list),
-    path('huerto_busqueda_simple',huerto_buscar)
+    path('huerto_busqueda_simple',huerto_buscar),
+    
     
 ]
--- Huerto/api_views.py
+++ Huerto/api_views.py
@@ -2,7 +2,9 @@ from .models import *
 from .serializers import *
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 from .forms import *
+from django.db.models import Q,Prefetch
 
 @api_view(['GET'])
 def huerto_list(request):
@@ -12,21 +14,15 @@ def huerto_list(request):
 
 @api_view(['GET'])
 def huerto_buscar(request):
-    huertos=Huerto.objects.all()
-    serializer=HuertoSerializerMejorado(huertos,many=True)
-    return Response(serializer.data)
-
-def huerto_buscar_cl(request):
-    formulario=BusquedaHuerto(request.GET)
-    headers={'Authorization': 'Bearer OjSzCkAWynmruDYbzT7MV4QAmavcV9'}
-    response = requests.get('http://127.0.0.1:4999/api/v1/huertos',headers=headers,params=formulario.data)
-    if formulario.is_valid():
-        texto= formulario.cleaned_data.get('textoBusqueda')
-        huertos = Huerto.objects.prefetch_related("usuario")
-        huertos = huertos.filter(sitio__startswith=texto)
-        mensaje_devuelto= "se busca por textos que empiezan la letra: " + texto
-        return render(request, 'huerto/lista_busqueda.html',{"huertos_mostrar":huertos,"texto_busqueda":texto})
-    if ("HTTP_REFERER"in request.META):
-        return redirect(request.META["HTTP_REFERER"])
+    if(request.user.has_perm("huerto.view_huerto")):
+        formulario=BusquedaHuerto(request.query_params)
+        if(formulario.is_valid()):
+            texto=formulario.data.get('textoBusqueda')
+            huertos = Huerto.objects.prefetch_related("usuario")
+            huertos = huertos.filter(sitio__startswith=texto).all()
+            serializer=HuertoSerializerMejorado(huertos,many=True)
+            return Response(serializer.data)
+        else:
+            return Response(formulario.errors, status=status.HTTP_400_BAD_REQUEST)
     else:
-        return redirect("index.html")
+        return Response({"Sin permisos"},status=status.HTTP_400_BAD_REQUEST)
